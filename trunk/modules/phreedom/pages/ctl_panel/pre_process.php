a987a7970442d8ddaece41f1fd891cd426f19392
phreesoft <dave.premo@phreesoft.com>
2013-10-28 15:34:14 -0600

    Fix for dashboard crash if language file is present but dashboard script is not.

--- trunk/modules/phreedom/pages/ctl_panel/pre_process.php
+++ trunk/modules/phreedom/pages/ctl_panel/pre_process.php
@@ -3,7 +3,6 @@
 // |                   PhreeBooks Open Source ERP                    |
 // +-----------------------------------------------------------------+
 // | Copyright(c) 2008-2013 PhreeSoft, LLC (www.PhreeSoft.com)       |
-
 // +-----------------------------------------------------------------+
 // | This program is free software: you can redistribute it and/or   |
 // | modify it under the terms of the GNU General Public License as  |
@@ -29,7 +28,7 @@ foreach ($dirs as $dir) {
   if (defined('MODULE_' . strtoupper($dir) . '_STATUS') && file_exists(DIR_FS_MODULES . $dir . '/dashboards/')) {
     $choices = scandir(DIR_FS_MODULES . "$dir/dashboards/");
 	foreach ($choices as $dashboard) {
-	  if ($dashboard == '.' || $dashboard == '..' || !is_dir(DIR_FS_MODULES."$dir/dashboards/".$dashboard)) continue;
+	  if ($dashboard == '.' || $dashboard == '..' || !file_exists(DIR_FS_MODULES."$dir/dashboards/$dashboard/$dashboard.php")) continue;
 	  $dashboards[] = array('module_id' => $dir, 'dashboard_id' => $dashboard);
 	}
   }
